6b3ddb7dfff0f83a2a4ebf780e475e1fc2c89670
Kyle Johnson <Brumous101@gmail.com>
2020-10-18 21:07:49 -0400

    Lab 1

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -5,6 +5,7 @@
 
 import math
 import itertools
+import time
 
 #I didn't use these but just brushed up on recursion with them
 def factorial(n):
@@ -23,26 +24,37 @@ def plus_or_minus_one(n):
 def derangements_1(n):
     if(n > 1):
         result = (n * derangements_1(n-1) + math.pow(-1, n))
-    else:
+    elif(n == 1):
         result = 0
+    elif(n == 0):
+        result = 1
+    else:
+        return "Error: Permutations must be a number of 0 or greater."
     return int(math.floor(result))
 
 def derangements_2(n):
-    return math.floor( ((math.factorial(n)) / (math.exp(1))) + (1/2) )
+    if(n > 1):
+        return math.floor( ((math.factorial(n)) / (math.exp(1))) + (1/2) )
+    elif(n == 1):
+        return 0
+    elif(n == 0):
+        return 1
+    else:
+        return "Error: Permutations must be a number of 0 or greater."
 
 def derangements_3(n):
     if(n > 1):
-        result = ((n-1) * ( derangements_4(n-1) + derangements_4(n-2) ) )
-    else:
+        result = ((n-1) * ( derangements_3(n-1) + derangements_3(n-2) ) )
+    elif(n == 1):
         result = 0
+    elif(n == 0):
+        result = 1
+    else:
+        return "Error: Permutations must be a number of 0 or greater."
     return result
 
 def derangements_4(n):
-    if(n == 0):
-        return 1
-    elif(n == 1):
-        return 0
-    else:
+    if(n > 1):
         originalList = create_list(n)
         permutationList = list(itertools.permutations(originalList))
         #print(originalList)
@@ -66,6 +78,13 @@ def derangements_4(n):
                     derangementCounter+=1
                 if( x == len(permutationList)):
                     break
+    elif(n == 1):
+        return 0
+    elif(n == 0):
+        return 1
+    else:
+        return "Error: Permutations must be a number of 0 or greater."
+        
     return derangementCounter 
 
 def create_list(n):
@@ -79,10 +98,26 @@ def create_list(n):
 
 val = input("Enter a number for the given amount of permutations to calculate the derangements: ")
 print("Derangements 1 based on our guess:")
+start_time = time.time()
 print(derangements_1(int(val)))
+end_time = time.time()
+print("Time elapsed: ") 
+print(end_time - start_time)
 print("Derangements 2 based inclusion and exclusion:")
+start_time = time.time()
 print(derangements_2(int(val)))
+end_time = time.time()
+print("Time elapsed: ") 
+print(end_time - start_time)
 print("Derangements 3 given 2 different possibilities of appearances for the value (secret santa/ peer grading):")
+start_time = time.time()
 print(derangements_3(int(val)))
+end_time = time.time()
+print("Time elapsed: ") 
+print(end_time - start_time)
 print("Derangements 4 based on counting:")
+start_time = time.time()
 print(derangements_4(int(val)))
+end_time = time.time()
+print("Time elapsed: ") 
+print(end_time - start_time)
